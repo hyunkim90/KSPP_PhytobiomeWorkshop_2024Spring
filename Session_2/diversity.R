b7360f8dd4e55d6cb37c543de96db61bbd7f08d3
ldm0420 <164974889+ldm0420@users.noreply.github.com>
2024-04-17 11:52:32 +0900

    Update diversity.R
    Add the relative abundance code

--- Session_2/diversity.R
+++ Session_2/diversity.R
@@ -1,6 +1,6 @@
 ######## 2024_Spring_KSPP workshop part.2
 ### 0. Install and load packages and set the working directory
-packages <- c("phyloseq", "vegan", "ggplot2", "lawstat", "FSA", "rcompanion")
+packages <- c("phyloseq", "vegan", "ggplot2", "lawstat", "FSA", "rcompanion", "dplyr", "remotes")
 install_packages <- packages[!sapply(packages, requireNamespace, quietly = TRUE)]
 if (length(install_packages) > 0) {
   install.packages(install_packages)
@@ -12,19 +12,17 @@ library(ggplot2)
 library(lawstat)
 library(FSA)
 library(rcompanion)
+library(dplyr)
 
 # Set the working directory
-setwd("C:/Users/WIN/Desktop/KSPP_PhytobiomeWorkshop_2024Spring-main")
-getwd()
-wd <- "./Session_1/Output/"
-setwd(wd)
+setwd("C:/Users/WIN/Desktop/KSPP_PhytobiomeWorkshop_2024Spring-main/Session_1")
 getwd()
 
 #### 1. Data preparation
 ## 1-1. Import biom, metadata, tree file
-a <- import_biom("otu_table_final.biom")
-b <- import_qiime_sample_data("sample_metadata.tsv")
-c <- read_tree("tree.nwk")
+a <- import_biom("./Output/4_Filter-sequence-table/otu_table_final.biom")
+b <- import_qiime_sample_data("./Input/sample_metadata.tsv")
+c <- read_tree("./Output/6_Build-Phylogeny/tree.nwk")
 
 # merge phyloseq object
 run <- merge_phyloseq(a,b,c)
@@ -51,7 +49,7 @@ rare.run <- rarefy_even_depth(run, rngseed = 1024)
 rare.run
 
 ## 1-4. Save the phyloseq object as .Rdata
-save.wd <- "./../../Session_2"
+save.wd <- "./../Session_2"
 setwd(save.wd)
 getwd()
 
@@ -205,8 +203,124 @@ ggplot(data = alpha.melt.wanted, aes(x=Branch_number, y=Value, fill=Branch_numbe
   theme(aspect.ratio = 1) +
   theme(panel.spacing=unit(1, "cm"))
 
-#### 3. Beta diversity
-## 3-1. Bray-curtis dissimilarity and PCoA ordination
+
+## 2-10. phylogenetic diversity
+install.packages("genefilter")
+remotes::install_github("twbattaglia/btools")
+library(btools)
+
+pd <- estimate_pd(rare.run)
+head(pd)
+pd$SampleID <- row.names(pd)
+pd$Branch_number <- sample_data(rare.run)$Branch_number
+rownames(pd) <- NULL
+head(pd)
+
+shapiro.test(pd$PD) # normality test
+kruskal.test(PD~Branch_number, data=pd) # Kruskal Wallis Test (nonparametric test)
+DT <- dunnTest(PD~Branch_number, data=pd)
+PT <- DT$res
+cld <- cldList(P.adj ~ Comparison,data = PT, threshold = 0.05)
+cld
+names(cld) <- c("Branch_number", "Letter", "MonoLetter")
+cld
+
+# plotting
+ggplot(data = pd, aes(x=Branch_number, y=PD, fill=Branch_number)) +
+  geom_boxplot(lwd=0.8, width = 0.8, outlier.shape=16) +
+  theme_bw() + scale_fill_manual(values=c("Branch_2"="#FF5765", "Branch_3"="#01DEE6",
+                                          "Branch_4"="#8A6FDF", "Branch_5"="#7CB53A")) +
+  #geom_point(position='jitter',shape=1, alpha=0.8, colour="black", size=1.5) + 
+  xlab("\n Branch") + ylab("Phylogenetic diversity \n") +
+  theme(axis.title.x = element_text(size = 15,hjust = 0.5, face='bold')) + 
+  theme(axis.title.y = element_text(size = 15,hjust = 0.5, face='bold')) + 
+  theme(axis.text.x = element_text(angle = 0, hjust = 0.5, vjust=0.4,size=12, face='bold',color='black'))+
+  theme(axis.text.y = element_text(size=15, face='bold',color='black'))+
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank()) +
+  theme(legend.position = "none") +
+  geom_text(data=cld, aes(label=Letter, x=Branch_number, y=2.6))
+
+#### 3. Relative abundance - bar plot
+## 3-1. make a dataframe for relative abundance calculation
+# check a sample data
+sample_data(run)
+#Branch_2, Branch_3, Branch_4, Branch_5
+
+# transform sample count and modifying the dataframe
+run.normalized <- transform_sample_counts(run, function(x) 100*x/sum(x)) # transform read counts so that total for each sample is 100.
+run.normalized.phylum <- tax_glom(run.normalized, "Phylum", NArm=FALSE) %>% psmelt() # extract only Phylum taxa and phyloseq data melting
+head(run.normalized.phylum) # check dataframe
+unique(run.normalized.phylum$Phylum) # check Phylum name and NA value, checking NA value any(is.na(df.phylum$Phylum))
+run.normalized.phylum$Phylum[is.na(run.normalized.phylum$Phylum)] <- "Unclassified" # change the NA value to Unclassified
+run.normalized.phylum$Phylum <- gsub("p__", "", run.normalized.phylum$Phylum)
+head(run.normalized.phylum)
+unique(run.normalized.phylum$Phylum)
+
+## 3-2. plot each sample
+library(RColorBrewer)
+my.colors <- brewer.pal(6, "Accent") # set color
+
+ggplot(run.normalized.phylum, aes(x=SampleID, y=Abundance, fill=Phylum)) +
+  geom_bar(stat="identity", width=0.8, position="stack") +
+  facet_wrap(~Branch_number, scales="free_x", ncol=2) +
+  theme_bw() +
+  theme(legend.position="right") +
+  theme(axis.title.x=element_text(size=20, face="bold")) +
+  theme(axis.title.y=element_text(size=20, face="bold")) +
+  theme(axis.text.x=element_text(size=12, face="bold")) +
+  theme(axis.text.y=element_text(size=12, face="bold")) +
+  theme(legend.title = element_text(size=15, face="bold")) +
+  theme(legend.text = element_text(size=12)) +
+  theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank()) +
+  theme(aspect.ratio = 1) +
+  labs(x="\nSampleID", y="Relative abundance (%)\n") +
+  scale_y_continuous(breaks=seq(0,100,20)) +
+  scale_fill_manual(values=my.colors)
+
+## 3-3. transform the counts to mean value by Phylum and each group
+run.normalized.phylum.mean <- run.normalized.phylum %>%
+  group_by(Branch_number, Phylum) %>%
+  summarize(Rel_abundance = mean(Abundance))
+head(run.normalized.phylum.mean)
+sum(run.normalized.phylum.mean$Rel_abundance)
+# 400
+
+# set sample order and phylum order
+order.sample <- c("Branch_2", "Branch_3", "Branch_4", "Branch_5")
+
+ord <- run.normalized.phylum.mean %>% group_by(Phylum) %>% summarise(Rel_abundance=sum(Rel_abundance)) %>% arrange(-Rel_abundance)
+vec <- ord$Phylum
+vec.charac <- as.character(vec)
+vec.order <- vec.charac[-which(vec.charac == "Unclassified")]
+vec.Low <- c("Unclassified")
+vec.reorder <- append(vec.order, vec.Low)
+vec.reorder
+
+run.normalized.phylum.mean$Branch_number <- factor(run.normalized.phylum.mean$Branch_number, levels=order.sample)
+run.normalized.phylum.mean$Phylum <- factor(run.normalized.phylum.mean$Phylum, levels=vec.reorder)
+
+## 3-4. plot each group
+ggplot(run.normalized.phylum.mean, aes(x=Branch_number, y=Rel_abundance, fill=Phylum)) +
+  geom_bar(stat="identity", width=0.8, position="stack") +
+  theme_bw() +
+  theme(legend.position="right") +
+  theme(axis.title.x=element_text(size=20, face="bold")) +
+  theme(axis.title.y=element_text(size=20, face="bold")) +
+  theme(axis.text.x=element_text(size=12, face="bold")) +
+  theme(axis.text.y=element_text(size=12, face="bold")) +
+  theme(legend.title = element_text(size=15, face="bold")) +
+  theme(legend.text = element_text(size=12)) +
+  theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank()) +
+  theme(aspect.ratio = 1) +
+  labs(x="\nBranch number", y="Relative abundance (%)\n") +
+  scale_y_continuous(breaks=seq(0,100,20)) +
+  scale_fill_manual(values=my.colors)
+
+#### 4. Beta diversity
+## 4-1. Bray-curtis dissimilarity and PCoA ordination
 ra = transform_sample_counts(run, function(x) 100 * x/sum(x))
 bray_distance <- phyloseq::distance(ra, "bray")
 ordu1 <- ordinate(run, "PCoA", "bray")
